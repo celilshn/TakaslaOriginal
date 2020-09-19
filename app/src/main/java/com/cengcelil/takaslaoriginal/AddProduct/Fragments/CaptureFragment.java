1043c5d20e451e82ab80876b164734e94f588cd3
celilshn <abdal.celil@gmail.com>
2020-09-20 01:06:15 +0300

    Yeni Ürün Ekleme : Focus ve diğer özellikler Hariç KAmera tamamlandı

--- app/src/main/java/com/cengcelil/takaslaoriginal/AddProduct/Fragments/CaptureFragment.java
+++ app/src/main/java/com/cengcelil/takaslaoriginal/AddProduct/Fragments/CaptureFragment.java
@@ -1,24 +1,25 @@
 package com.cengcelil.takaslaoriginal.AddProduct.Fragments;
 
-import android.hardware.Camera;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.camerakit.CameraKit;
+import com.camerakit.CameraKitView;
+import com.camerakit.type.CameraFlash;
 import com.cengcelil.takaslaoriginal.Adapters.CapturedAdapter;
 import com.cengcelil.takaslaoriginal.Models.CapturedItem;
-import com.cengcelil.takaslaoriginal.Models.ShowCamera;
 import com.cengcelil.takaslaoriginal.R;
 
 import java.io.File;
@@ -28,46 +29,29 @@ import java.util.ArrayList;
 
 public class CaptureFragment extends Fragment {
     private static final String TAG = "CaptureFragment";
-    private static Camera camera;
-    private ShowCamera showCamera;
-    private FrameLayout flCamera;
-    private Camera.PictureCallback pictureCallback;
     private RecyclerView capturedRecycler;
     private CapturedAdapter capturedAdapter;
     private ArrayList<CapturedItem> capturedItems;
     private ImageView turnButton;
-    private boolean isFront = false;
-    private View view;
+    private CameraKitView cameraKitView;
+    private ImageView flashButton;
+    private static boolean isFlashOn = false;
 
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        cameraKitView.onStart();
+        cameraKitView.setSensorPreset(CameraKit.SENSOR_PRESET_NONE);
+        cameraKitView.setImageMegaPixels(10f);
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate: ");
         capturedItems = new ArrayList<>();
-        pictureCallback = new Camera.PictureCallback() {
-            @Override
-            public void onPictureTaken(byte[] data, Camera camera) {
-                Log.d(TAG, "onPictureTaken: ");
-                File file = getOutputMediaFiles();
-                if (file == null)
-                    return;
-                else {
-                    Log.d(TAG, "onPictureTaken: writing");
-                    try {
-                        capturedItems.add(0, new CapturedItem(file));
-                        capturedAdapter.notifyDataSetChanged();
-                        FileOutputStream fos = new FileOutputStream(file);
-                        fos.write(data);
-                        fos.close();
-                        camera.startPreview();
-                    } catch (IOException e) {
-                        Log.d(TAG, "onPictureTaken: failed" + e.getMessage());
-                        e.printStackTrace();
-                    }
-                }
-            }
-        };
+
     }
 
     @Override
@@ -82,7 +66,9 @@ public class CaptureFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         Log.d(TAG, "onViewCreated: ");
-        flCamera = view.findViewById(R.id.flCamera);//  1
+        cameraKitView = view.findViewById(R.id.camera);//
+        flashButton = view.findViewById(R.id.capture_top_bar_flash_button);
+// 1
         turnButton = view.findViewById(R.id.capture_top_bar_turn_button);// 1
         capturedRecycler = view.findViewById(R.id.captured_recyclerview);// 1
 
@@ -99,10 +85,29 @@ public class CaptureFragment extends Fragment {
         turnButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (isFront)
-                    switchToBackCamera();
-                else
-                    switchToFrontCamera();
+                if (cameraKitView.getFacing() == CameraKit.FACING_BACK)
+                    cameraKitView.setFlash(CameraKit.FLASH_OFF);
+                cameraKitView.toggleFacing();
+            }
+        });
+        flashButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: ");
+                if (cameraKitView.getFacing() == CameraKit.FACING_BACK)
+                    if (isFlashOn) {
+                        Log.d(TAG, "onClick: flash on to off");
+                        isFlashOn = false;
+                        flashButton.setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.capture_top_bar_flash_on_button));
+                        cameraKitView.setFlash(CameraKit.FLASH_OFF);
+                    } else {
+                        Log.d(TAG, "onClick: flash off to on");
+                        isFlashOn = true;
+                        flashButton.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.capture_top_bar_flash_off_button));;
+                        cameraKitView.setFlash(CameraKit.FLASH_TORCH);
+
+
+                    }
             }
         });
     }
@@ -111,23 +116,21 @@ public class CaptureFragment extends Fragment {
     public void onResume() {
         super.onResume();
         Log.d(TAG, "onResume: ");
-        if (camera == null)
-            if (isFront)
-                camera = Camera.open(Camera.CameraInfo.CAMERA_FACING_FRONT);
-            else
-                camera = Camera.open(Camera.CameraInfo.CAMERA_FACING_BACK);
-        showCamera = new ShowCamera(getActivity(), camera);
-        flCamera.addView(showCamera);
-        showCamera.onResume();
+        cameraKitView.onResume();
     }
 
     @Override
     public void onPause() {
         super.onPause();
         Log.d(TAG, "onPause: ");
-        stopCamera();
+        cameraKitView.onPause();
     }
 
+    @Override
+    public void onStop() {
+        super.onStop();
+        cameraKitView.onStop();
+    }
 
     private File getOutputMediaFiles() {
         Log.d(TAG, "getOutputMediaFiles: ");
@@ -139,47 +142,35 @@ public class CaptureFragment extends Fragment {
                     Environment.DIRECTORY_PICTURES);
             if (folder.exists())
                 folder.mkdirs();
-            return new File(folder, String.valueOf(System.currentTimeMillis()) + ".jpg");
+            return new File(folder, System.currentTimeMillis() + ".jpg");
         }
     }
 
-
-    private void switchToFrontCamera() {
-        Log.d(TAG, "switchToFrontCamera: ");
-        isFront = true;
-        stopCamera();
-        camera = Camera.open(Camera.CameraInfo.CAMERA_FACING_FRONT);
-        showCamera.setCamera(camera);
-        flCamera.addView(showCamera);
-        showCamera.onResume();
-    }
-
-    private void switchToBackCamera() {
-        Log.d(TAG, "switchToBackCamera: ");
-        isFront = false;
-        stopCamera();
-        camera = Camera.open(Camera.CameraInfo.CAMERA_FACING_BACK);
-        showCamera.setCamera(camera);
-        flCamera.addView(showCamera);
-        showCamera.onResume();
-    }
 
     public void captureImage() {
         Log.d(TAG, "captureImage: ");
-        if (camera != null) {
-            camera.takePicture(null, null, pictureCallback);
-        }
+        cameraKitView.captureImage(new CameraKitView.ImageCallback() {
+            @Override
+            public void onImage(CameraKitView cameraKitView, final byte[] bytes) {
+                Log.d(TAG, "onImage: ");
+                File file = getOutputMediaFiles();
+                if (file == null)
+                    return;
+                else {
+                    Log.d(TAG, "onPictureTaken: writing");
+                    try {
+                        capturedItems.add(0, new CapturedItem(file));
+                        capturedAdapter.notifyDataSetChanged();
+                        FileOutputStream fos = new FileOutputStream(file);
+                        fos.write(bytes);
+                        fos.close();
+                    } catch (IOException e) {
+                        Log.d(TAG, "onPictureTaken: failed" + e.getMessage());
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
     }
 
-    public void stopCamera() {
-        Log.d(TAG, "stopCamera: ");
-        if (camera != null) {
-            Log.d(TAG, "stopCamera: camera !=null");
-            flCamera.removeAllViews();
-            camera.setPreviewCallback(null);
-            camera.release();
-            camera = null;
-        } else
-            Log.d(TAG, "stopCamera: camera==null");
-    }
 }
