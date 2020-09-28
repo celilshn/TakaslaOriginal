d4119facd7538b6ffba70580a7dc676063eb8562
celilshn <abdal.celil@gmail.com>
2020-09-29 01:35:12 +0300

    Yeni Ürün Ekleme : Ürün Özellikleri Sayfası Giriş

---
+++ app/src/main/java/com/cengcelil/takaslaoriginal/AddProduct/Fragments/DetailsFragment.java
@@ -0,0 +1,34 @@
+package com.cengcelil.takaslaoriginal.AddProduct.Fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cengcelil.takaslaoriginal.R;
+
+public class DetailsFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_details, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
